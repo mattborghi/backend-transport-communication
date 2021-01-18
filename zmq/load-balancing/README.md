cf3846d5e6e3ebf26031fc7e7c83e639982967e5
Matias Borghi <borghi.matias@gmail.com>
2021-01-18 16:57:50 -0300

    add simple REQ-ROUTER load balancing example

--- zmq/load-balancing/README.md
+++ zmq/load-balancing/README.md
@@ -21,3 +21,38 @@ Anywhere you can use REQ, you can use DEALER. There are two specific differences
 
 The synchronous versus asynchronous behavior has no effect on our example because we’re doing strict request-reply. It is more relevant when we address recovering from failures.
 
+## Load balancing message broker
+
+The previous example is half-complete. It can manage a set of workers with dummy requests and replies, but it has no way to talk to clients. If we add a second frontend ROUTER socket that accepts client requests, and turn our example into a proxy that can switch messages from frontend to backend, we get a useful and reusable tiny load balancing message broker.
+
+![imag](https://zguide.zeromq.org/images/fig32.png)
+
+### Message envelope
+
+Let’s assume the client’s identity is “CLIENT” and the worker’s identity is “WORKER”. The client application sends a single frame containing “Hello”.
+
+![imag](https://zguide.zeromq.org/images/fig33.png)
+
+**Figure 1**: Message that Client sends.
+
+Because the REQ socket adds its empty delimiter frame and the ROUTER socket adds its connection identity, the proxy reads off the frontend ROUTER socket the client address, empty delimiter frame, and the data part.
+
+![imag](https://zguide.zeromq.org/images/fig34.png)
+
+**Figure 2**: Message coming in on Frontend.
+
+The broker sends this to the worker, prefixed by the address of the chosen worker, plus an additional empty part to keep the REQ at the other end happy.
+
+![imag](https://zguide.zeromq.org/images/fig35.png)
+
+**Figure 3**: Message sent to Backend.
+
+This complex envelope stack gets chewed up first by the backend ROUTER socket, which removes the first frame. Then the REQ socket in the worker removes the empty part, and provides the rest to the worker application.
+
+![imag](https://zguide.zeromq.org/images/fig36.png)
+
+**Figure 4**: Message delivered to Worker.
+
+The worker has to save the envelope (which is all the parts up to and including the empty message frame) and then it can do what’s needed with the data part. Note that a REP socket would do this automatically, but we’re using the REQ-ROUTER pattern so that we can get proper load balancing.
+
+On the return path, the messages are the same as when they come in, i.e., the backend socket gives the broker a message in five parts, and the broker sends the frontend socket a message in three parts, and the client gets a message in one part.
---
+++ zmq/load-balancing/lbbroker.py
@@ -0,0 +1,108 @@
+# Load-balancing broker
+#
+# Clients and workers are shown here in-process.
+#
+
+# from __future__ import print_function
+
+from multiprocessing import Process
+
+import zmq
+
+
+NBR_CLIENTS = 10
+NBR_WORKERS = 3
+
+
+def client_task(ident):
+    """Basic request-reply client using REQ socket."""
+    socket = zmq.Context().socket(zmq.REQ)
+    socket.identity = u"Client-{}".format(ident).encode("ascii")
+    socket.connect("ipc://frontend.ipc")
+
+    # Send request, get reply
+    socket.send(b"HELLO")  # Figure 1: one-part messages
+    reply = socket.recv()
+    print("{}: {}".format(socket.identity.decode("ascii"),
+                          reply.decode("ascii")))
+
+
+def worker_task(ident):
+    """Worker task, using a REQ socket to do load-balancing."""
+    socket = zmq.Context().socket(zmq.REQ)
+    socket.identity = u"Worker-{}".format(ident).encode("ascii")
+    socket.connect("ipc://backend.ipc")
+
+    # Tell broker we're ready for work
+    socket.send(b"READY")
+
+    while True:
+        address, empty, request = socket.recv_multipart()  # Figure 4: three-parts messages
+        print("{}: {}".format(socket.identity.decode("ascii"),
+                              request.decode("ascii")))
+        socket.send_multipart([address, b"", b"OK"])
+
+
+def main():
+    """Load balancer main loop."""
+    # Prepare context and sockets
+    context = zmq.Context.instance()
+    frontend = context.socket(zmq.ROUTER)
+    frontend.bind("ipc://frontend.ipc")
+    backend = context.socket(zmq.ROUTER)
+    backend.bind("ipc://backend.ipc")
+
+    # Start background tasks
+    def start(task, *args):
+        process = Process(target=task, args=args)
+        process.daemon = True
+        process.start()
+    for i in range(NBR_CLIENTS):
+        start(client_task, i)
+    for i in range(NBR_WORKERS):
+        start(worker_task, i)
+
+    # Initialize main loop state
+    count = NBR_CLIENTS
+    workers = []
+    poller = zmq.Poller()
+    # Only poll for requests from backend until workers are available
+    poller.register(backend, zmq.POLLIN)
+
+    while True:
+        sockets = dict(poller.poll())
+
+        if backend in sockets:
+            # Handle worker activity on the backend
+            request = backend.recv_multipart()  # Figure 3: Five-part messages
+            worker, empty, client = request[:3]  # first 3 elems
+            if not workers:
+                # Poll for clients now that a worker is available
+                poller.register(frontend, zmq.POLLIN)
+            workers.append(worker)
+            if client != b"READY" and len(request) > 3:
+                # If client reply, send rest back to frontend
+                empty, reply = request[3:]
+                frontend.send_multipart([client, b"", reply])
+                count -= 1
+                if not count:
+                    break
+
+        if frontend in sockets:
+            # Get next client request, route to last-used worker
+            client, empty, request = frontend.recv_multipart()  # Figure 2
+            worker = workers.pop(0)
+            backend.send_multipart(
+                [worker, b"", client, b"", request])  # Figure 3
+            if not workers:
+                # Don't poll clients if no workers are available
+                poller.unregister(frontend)
+
+    # Clean up
+    backend.close()
+    frontend.close()
+    context.term()
+
+
+if __name__ == "__main__":
+    main()
